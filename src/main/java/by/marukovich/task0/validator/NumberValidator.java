dd0cee392fe79a4d1aaf6d78437225400eb8158a
Maksim <maksim.marukovich.00@mail.ru>
2022-02-04 10:42:49 +0300

    some corrections on code

--- src/main/java/by/marukovich/task0/validator/NumberValidator.java
+++ src/main/java/by/marukovich/task0/validator/NumberValidator.java
@@ -1,33 +1,26 @@
 package by.marukovich.task0.validator;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class NumberValidator {
     private static final NumberValidator instance = new NumberValidator();
-    public static final Logger log = LogManager.getLogger(NumberValidator.class);
-    private static final String SPLIT = "\\s+";
+    public static final Logger log = LogManager.getLogger();
+    private static final String REGEXP_STRING_VALIDATOR = "^\\s*-?\\d+(\\s+(-?\\d+))*\\s*$";
 
     public static NumberValidator getInstance() {
         return instance;
     }
 
     public boolean validateStringWithIntegers(String str) {
-        String[] literals = str.strip().split(SPLIT);
-        return AllNumbersInt(literals);
-    }
-
-    private boolean AllNumbersInt(String[] literals) {
-        for (String literal : literals) {
-            try {
-                Integer.parseInt(literal.strip());
-
-            } catch (NumberFormatException e) {
-                log.error("literal " + literal + " can't be parsed to int",e);
-                return false;
-            }
+        if (str != null && str.matches(REGEXP_STRING_VALIDATOR)) {
+            return true;
+        } else {
+            log.log(Level.INFO, "String {} can not be parsed to int", str);
+            return false;
         }
-        return true;
-
     }
+
+
 }
